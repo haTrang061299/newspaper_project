4cbd07b9d03f6f299caaa06a16c3b48b64d72f74
haTrang061299 <33522506+haTrang061299@users.noreply.github.com>
2018-11-27 15:42:49 -0500

    Add files via upload

---
+++ articles/views.py
@@ -0,0 +1,48 @@
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
+from .models import Article
+
+class ArticleListView(ListView, LoginRequiredMixin):
+    model = Article
+    template_name = 'article_list.html'
+
+class ArticleDetailView(DetailView, LoginRequiredMixin):
+    model = Article
+    template_name = 'article_detail.html'
+
+class ArticleUpdateView(UpdateView, LoginRequiredMixin):
+    model = Article
+    fields = ('title', 'body')
+    template_name = 'article_edit.html'
+    login_url = 'login'
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            raise PermissionDenied
+        return super().dispatch(request, *args, **kwargs)
+
+class ArticleDeleteView(DeleteView, LoginRequiredMixin):
+    model = Article
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('article_list')
+    login_url = 'login'
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            raise PermissionDenied
+        return super().dispatch(request, *args, **kwargs)
+
+class ArticleCreateView(CreateView, LoginRequiredMixin):
+    model = Article
+    template_name = 'article_new.html'
+    fields = ('title', 'body')
+    login_url = 'login'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
---
+++ newspaper_project/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic.base import TemplateView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
+    path('users/', include('django.contrib.auth.urls')),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    path('', include('pages.urls')),
+    path('articles/', include('articles.urls')),
+]
